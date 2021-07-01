694fffb98c252fc09c3e4390f493c34261602f50
Hailey <hailey@MacBook-Pro.local>
2021-07-01 14:01:31 -0400

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,14 +4,42 @@ import styled from 'styled-components'
 import ContactForm from '../components/contactForm'
 import bigTop from '../images/bigTop.jpg'
 import rod from '../images/rod.jpg'
+
+
+
 const Content = styled.div`
-/* height: 20vw; */
+
 width: 100%;
 z-index: 1;
-margin-top: 27%;
-margin-left: 33%;
+margin-top: 28%;
+margin-left: 32%;
 /* height: 5vw; */
 position: relative;
+
+`
+const Content1 = styled.div`
+p{
+  margin-bottom: 0.3rem;
+}
+
+h3{
+  margin-bottom: 0.3rem;
+}
+justify-content: center;
+margin-left: 42%;
+border-radius: 15px;
+font-size: 12px;
+align-items: center;
+margin-top: 1rem;
+background: #DBBF96;
+height: 5rem;
+width: 15rem;
+position: relative;
+text-align: center;
+box-shadow: 2px 2px 15px white;
+  filter: drop-shadow(1px 1px 1px black);
+
+
 `
 const BigTop = styled.div`
 position: fixed;
@@ -29,7 +57,7 @@ img{
 
 }
 h1{
-  margin-top: -10rem;
+  margin-top: -15rem;
   margin-left: 38%;
 color:white;
 font-size: 4rem;
@@ -40,14 +68,27 @@ const Contact = ({children}) => {
   return(
     <>
 <Layout>
+
 <BigTop>
   <img src={rod}></img>
   <h1>Contact Us: </h1>
+  <Content1>
+  <div>
+    <h3>phone: </h3>
+    <p>850-510-5724</p>
+  </div>
+  <div>
+    <h3>email: </h3>
+    <p>swampyankee57@gmail.com</p>
+  </div>
+  </Content1>
   </BigTop>
 <Content>
 <ContactForm/>
 </Content>
+
 </Layout>
+
     </>
   )
 }
